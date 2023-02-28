b0cd586b237571b37431af0cceba39b68a8d8862
keshoxvol <51890346+keshoxvol@users.noreply.github.com>
2023-02-28 15:47:17 +0300

    ss

---
+++ src/main/java/ru/codovstvo/stat/Classes/Info.java
@@ -0,0 +1,117 @@
+package ru.codovstvo.stat.Classes;
+
+public class Info { 
+
+    private String userId;
+
+    private String startDateSession;
+
+    private String startTimeSession;
+
+    private String endDateSession;
+
+    private String endTimeSession;
+
+    private String leghtSession;
+
+    private int countSession;
+
+
+    private int wastesMoney;
+
+    private int wastesRuby;
+
+    private int wastesEnergy;
+
+    private int wastesFruit;
+
+    private int wastesApple;
+
+    private int wastesTangerine;
+
+    private int wastesRaspberry;
+
+    private int wastesKey;
+
+
+    private int earnedExpirience;
+    
+    private int earnedMoney;
+
+    private int earnedRuby;
+
+    private int earnedEnergy;
+
+    private int earnedFruit;
+
+    private int earnedApple;
+
+    private int earnedTangerine;
+
+    private int earnedRaspberry;
+
+    private int earnedKey;
+
+    
+    private int activeOrder;
+    
+    private int collectOrder;
+
+
+    private int successQuest;
+
+    private int successDailyTask;
+
+
+    private String countBuyItemShopMoney;
+
+    private String countBuyItemShopRuby;
+
+    private String countBuyItemShopEnergy;
+
+    private String countBuyItemShopWorker;
+
+    private String countBuyItemShopFruit;
+
+    private String countBuyItemShopResources;
+
+    private String countBuyItemShopSales;
+
+    private String countBuyItemShopOrder;
+
+    private String indexBuyItemShopMoney;
+
+    private String clickBuyItemShopRuby;
+
+    private String indexBuyItemShopRuby;
+
+    private String indexBuyItemShopEnergy;
+
+    private String indexBuyItemShopWorker;
+
+    private String indexBuyItemShopFruit;
+
+    private String indexBuyItemShopResources;
+    
+    private String indexBuyItemShopSales;
+
+
+    private int clickAD;
+
+    private int successAD;
+
+    private int errorAD;
+    
+
+    private int clickDonate;
+
+    private int successDonate;
+
+    private int redirectCount;
+
+    private String typeClickDonate;
+
+    private String timeClickDonate;
+
+    private String timeSpawnClouds;
+}
--- src/main/java/ru/codovstvo/stat/services/SessionInfoService.java
+++ src/main/java/ru/codovstvo/stat/services/SessionInfoService.java
@@ -2,17 +2,33 @@ package ru.codovstvo.stat.services;
 
 import org.springframework.stereotype.Service;
 
-import org.json.JSONObject;  
+
+
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;  
 
 @Service
 public class SessionInfoService {
     
     public void main(String reqString){
+
         JSONObject json = new JSONObject(reqString);  
 
         System.out.println("----------------------");
         System.out.println("session: " + json.getString("countSeccion"));
         System.out.println("lengthSeccion: " + json.getString("lengthSeccion"));
+        System.out.println("----------------------");
+    }
+
+
+    public void main2(String reqString) throws ParseException{
+        Object obj = new JSONParser().parse(reqString);
+        JSONObject jo = (JSONObject) obj;
+
+        System.out.println("----------------------");
+        System.out.println("session: " + jo.getString("countSeccion"));
+        System.out.println("lengthSeccion: " + jo.getString("lengthSeccion"));
         System.out.println("----------------------");
     }
 }
